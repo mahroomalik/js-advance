77bd236a0668ca34c288580bfa1ed68653fd64f0
mahroomalik <123258241+mahroomalik@users.noreply.github.com>
2023-09-06 08:16:15 +0500

    Add files via upload

---
+++ inputfile.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+var fname = prompt("enter you first name: ");
+var lname = prompt("enter you last name: ");
+console.log("your full name is", fname,lname);
---
+++ isArray.js
@@ -0,0 +1,10 @@
+        
+var a = [ "mahroo", "malik", " sara", "sana", "tania"];
+var b = 30.1;
+var c = "mahroo-malik";
+if(Array.isArray(c)) {
+    console.log("this is an array");
+}else{
+    console.log("this is not an array");
+}
+
